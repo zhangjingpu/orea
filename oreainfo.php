82562be2c9be702d1d1a01af26b48b09268e4ce4
Reage <reaten@126.com>
2012-11-18 16:01:15 +0800

    add oreainfo.php

---
+++ oreainfo.php
@@ -0,0 +1,10 @@
+<?php
+define ('Version','1.0');
+define ('Url', 'http://orea.daruisoft.com');
+define ('CODING_TYPE','gb2312');
+/*
+ * OREA_PATH为dics的目录。如C:\orea\dics
+ * OREA_PATH is the floder where stores dic db, eg: C:\orea\dics
+ */
+define ('OREA_PATH', '%Path%');
+?>
